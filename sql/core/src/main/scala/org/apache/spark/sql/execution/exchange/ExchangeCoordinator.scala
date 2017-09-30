350af8745b0af8732ece87d4b0debe2df40051de
GuoChenzhao <chenzhao.guo@intel.com>
2017-09-30 14:56:59 +0800

    Refactor

--- sql/core/src/main/scala/org/apache/spark/sql/execution/exchange/ExchangeCoordinator.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/exchange/ExchangeCoordinator.scala
@@ -172,6 +172,15 @@ class ExchangeCoordinator(
       (size, rowCount)
     }
 
+    def isLargerThanTarget(
+      now: Tuple2[Long, Long], plus: Tuple2[Long, Long], targetSize: Long, targetRowCount: Long) = {
+      now._1 + plus._1 > targetSize || now._2 + plus._2 > targetRowCount
+    }
+
+    def increaseSizeAndRowCount(now: Tuple2[Long, Long], plus: Tuple2[Long, Long]) = {
+      (now._1 + plus._1, now._2 + plus._2)
+    }
+
     val firstStartIndex = nextStartIndex(0)
     partitionStartIndices += firstStartIndex
     var postShuffleInput = partitionSizeAndRowCount(firstStartIndex)
@@ -182,16 +191,14 @@ class ExchangeCoordinator(
       val nextShuffleInput = partitionSizeAndRowCount(nextIndex)
       // If the next partition is omitted, or including the nextShuffleInputSize would exceed the
       // target partition size, then start a new partition.
-      if (nextIndex != i + 1 ||
-        postShuffleInput._1 +  nextShuffleInput._1 > targetPostShuffleInputSize ||
-        postShuffleInput._2 + nextShuffleInput._2 > targetPostShuffleRowCount) {
+      if (nextIndex != i + 1 || isLargerThanTarget(postShuffleInput, nextShuffleInput,
+        targetPostShuffleInputSize, targetPostShuffleRowCount)) {
         partitionEndIndices += i + 1
         partitionStartIndices += nextIndex
         postShuffleInput = nextShuffleInput
         i = nextIndex
       } else {
-        postShuffleInput = (postShuffleInput._1 + nextShuffleInput._1,
-          postShuffleInput._2 + nextShuffleInput._2)
+        postShuffleInput = increaseSizeAndRowCount(postShuffleInput, nextShuffleInput)
 
         i += 1
       }
